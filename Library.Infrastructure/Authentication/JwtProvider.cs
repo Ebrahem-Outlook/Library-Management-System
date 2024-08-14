06429957a2d52de128f1d5c1d9146f3907a24249
EbrahemElbetar1@outlook.com <EbrahemElbetar1@outlook.com>
2024-08-14 13:59:41 +0300

    V_12

---
+++ Library.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using Library.Infrastructure.Authentication.Settings;
+
+namespace Library.Infrastructure.Authentication;
+
+internal class JwtProvider 
+{
+    private readonly JwtSettings _jwtSettings;
+
+   
+}
